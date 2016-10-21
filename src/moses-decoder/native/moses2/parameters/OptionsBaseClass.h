6937904802afa8d7b04deda1a9da787d336dbe20
David Madl <git@abanbytes.eu>
2016-10-21 12:43:06 +0200

    copy moses2 into repository
    This is based on mosesdecoder master 883cbc2a4f6e955fe2e94d20fd541a0ab721fa3b
    
    plus cmake, draft PhraseTableSADB integration: 71a33d49a101bdc6c0fed8ebfd229cfad06640b8
    (cidermole)

---
+++ src/moses-decoder/native/moses2/parameters/OptionsBaseClass.h
@@ -0,0 +1,21 @@
+// -*- mode: c++; indent-tabs-mode: nil; tab-width: 2 -*-
+#pragma once
+
+#include <string>
+#include <map>
+
+namespace Moses2
+{
+
+class Parameter;
+
+struct OptionsBaseClass
+{
+};
+
+}
+
+namespace xmlrpc_c
+{
+typedef std::string value;
+}
